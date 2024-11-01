5ce953a50e5a0d8abc27c010a8331f31ecfd115a
Voris03 <v.vidus03@gmail.com>
2024-11-01 14:02:20 +0300

    fix: foldNum && add: deductNum

--- src/Counter/Counter.tsx
+++ src/Counter/Counter.tsx
@@ -1,18 +1,21 @@
 import React, { useState } from "react";
 
 export const Counter = () => {
+    const [num, setNum] = useState(0);
 
-    const [num, setNum] = useState(0)
+    const foldNum = () => {
+        setNum(prevNum => prevNum + 1);
+    };
 
-    const changeNum = (num: number) => {
-        debugger
-        setNum(num+1)
-    }
+    const deductNum = () => {
+        setNum(prevNum => prevNum - 1);
+    };
 
     return (
         <div>
             {num}
-            <button onClick={() => {changeNum(num)}}>+</button>
+            <button onClick={foldNum}>+</button>
+            <button onClick={deductNum}>-</button>
         </div>
-    )
-}
+    );
+};
